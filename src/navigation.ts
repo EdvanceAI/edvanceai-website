cb86484531d6fa306ae6884367fc21bd4b880b08
Parim Shah <shahparim3@gmail.com>
2024-11-27 03:52:16 -0500

    tittle change

--- src/navigation.ts
+++ src/navigation.ts
@@ -7,107 +7,112 @@ export const headerData = {
       href: getPermalink('/')
     },
     {
-      text: 'Pages',
-      links: [
-        {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
-        },
-        {
-          text: 'Services',
-          href: getPermalink('/services'),
-        },
-        {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
-        },
-        {
-          text: 'About us',
-          href: getPermalink('/about'),
-        },
-        {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
-        },
-        {
-          text: 'SaaS',
-          href: getPermalink('/homes/sas'),
-        },
-        {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
-        },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
-        },
-      ],
+      text: "Our Team",
+      href: getPermalink('/about')
     },
     {
-      text: 'Blog',
-      links: [
-        {
-          text: 'Blog List',
-          href: getBlogPermalink(),
-        },
-        {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
-        {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
-      ],
+      text: "Contact",
+      href: getPermalink('/contact')
     },
-    {
-      text: 'Widgets',
-      href: '#',
-    },
+    
+//     {
+//       text: 'Pages',
+//       links: [
+//         {
+//           text: 'Features (Anchor Link)',
+//           href: getPermalink('/#features'),
+//         },
+//         {
+//           text: 'Services',
+//           href: getPermalink('/services'),
+//         },
+//         {
+//           text: 'Pricing',
+//           href: getPermalink('/pricing'),
+//         },
+//         {
+//           text: 'About us',
+//           href: getPermalink('/about'),
+//         },
+//         {
+//           text: 'Contact',
+//           href: getPermalink('/contact'),
+//         },
+//         {
+//           text: 'Terms',
+//           href: getPermalink('/terms'),
+//         },
+//         {
+//           text: 'Privacy policy',
+//           href: getPermalink('/privacy'),
+//         },
+//         {
+//           text: 'SaaS',
+//           href: getPermalink('/homes/sas'),
+//         },
+//         {
+//           text: 'Startup',
+//           href: getPermalink('/homes/startup'),
+//         },
+//         {
+//           text: 'Mobile App',
+//           href: getPermalink('/homes/mobile-app'),
+//         },
+//         {
+//           text: 'Personal',
+//           href: getPermalink('/homes/personal'),
+//         },
+//       ],
+//     },
+//     {
+//       text: 'Blog',
+//       links: [
+//         {
+//           text: 'Blog List',
+//           href: getBlogPermalink(),
+//         },
+//         {
+//           text: 'Article',
+//           href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
+//         },
+//         {
+//           text: 'Article (with MDX)',
+//           href: getPermalink('markdown-elements-demo-post', 'post'),
+//         },
+//         {
+//           text: 'Category Page',
+//           href: getPermalink('tutorials', 'category'),
+//         },
+//         {
+//           text: 'Tag Page',
+//           href: getPermalink('astro', 'tag'),
+//         },
+//         {
+//           text: 'Lead Generation',
+//           href: getPermalink('/landing/lead-generation'),
+//         },
+//         {
+//           text: 'Long-form Sales',
+//           href: getPermalink('/landing/sales'),
+//         },
+//         {
+//           text: 'Click-Through',
+//           href: getPermalink('/landing/click-through'),
+//         },
+//         {
+//           text: 'Product Details (or Services)',
+//           href: getPermalink('/landing/product'),
+//         },
+//         {
+//           text: 'Coming Soon or Pre-Launch',
+//           href: getPermalink('/landing/pre-launch'),
+//         },
+//         {
+//           text: 'Subscription',
+//           href: getPermalink('/landing/subscription'),
+//         },
+//       ],
+//     },
   ],
   actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
 };
@@ -115,15 +120,9 @@ export const headerData = {
 export const footerData = {
   links: [
     {
-      title: 'Product',
+      title: 'About',
       links: [
         { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
       ],
     },
     {
@@ -164,10 +163,9 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+    { ariaLabel: 'Discord', icon: 'tabler:brand-discord', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'Linktree', icon: 'tabler:brand-linktree', href: 'https://github.com/onwidget/astrowind' },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
   footNote: `
