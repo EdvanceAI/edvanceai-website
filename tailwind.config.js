d44de217e650dfc59453c68ac802f9e4bca52cad
Parim Shah <shahparim3@gmail.com>
2024-12-07 01:38:01 -0500

    styling fix and analytics

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,6 @@ export default {
         primary: 'var(--aw-color-primary)',
         secondary: 'var(--aw-color-secondary)',
         accent: 'var(--aw-color-accent)',
-        default: 'var(--aw-color-text-default)',
         muted: 'var(--aw-color-text-muted)',
       },
       fontFamily: {
